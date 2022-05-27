712df84bf446f54347ae56d82f46f238fd78513b
João Paulo Santos <joaopaulosantos418@gmail.com>
2022-05-27 10:45:32 -0300

    tela bem vindo

---
+++ app/src/pages/bemVindo/style.tsx
@@ -0,0 +1,27 @@
+
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container:{
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 32,
+        paddingHorizontal: 40
+    },
+    header:{
+        width: '100%',
+        marginTop:50,
+        marginBottom: 75
+    },
+    middle: {
+        marginTop: 75,
+        width: '100%'
+    },
+    middle2: {
+        width: '100%',
+        paddingHorizontal: 50,
+        alignItems: 'center',
+        marginTop: 30,
+        marginBottom: 100
+    }
+});
