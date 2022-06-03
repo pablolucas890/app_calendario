17f7341ba6a4ceb3e8b11ee5f357a0e43d830a0a
pablolucas890 <pablolucas890@gmail.com>
2022-06-03 10:01:31 -0300

    Arrumando cor do botao2

--- app/src/components/Back/style.tsx
+++
@@ -1,9 +0,0 @@
-
-import { StyleSheet } from 'react-native';
-import { color } from '../../util/color';
-
-export const styles = StyleSheet.create({
-    container: {
-        backgroundColor: color.verde,
-    }
-});
