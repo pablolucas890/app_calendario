71c5de1ce87723bddaf40c50fd7f7b82898c06ab
Maria Sanchez <clarasanchez2398@gmail.com>
2022-06-22 11:56:17 -0300

    Inserimos a data atual
    Co-Authored-By:Jadyla <jadylacesariof@gmail.com>
    Co-Authored-By:Samara <samara.quintiliano76@gmail.com>
    Co-Authored-By: João Paulo <joaopaulosantos418@gmail.com>
    Co-Authored-By: Gabriel <gabriel.boloks@gmail.com>
    Co-Authored-By: Igor <igor_paula12@hotmail.com>

--- app/src/pages/calendario/index.tsx
+++ app/src/pages/calendario/index.tsx
@@ -1,55 +1,23 @@
+import { View } from 'react-native';
 import { Calendar } from 'react-native-calendario';
+import { styles } from './style';
+
+
+let date = new Date().getDate();
+let month = new Date().getMonth();
+let year = new Date().getFullYear();
+
 export function Calendario() {
   return(
-    <Calendar
-    
-      onPress={(range) => console.log(range)}
-      minDate={new Date(2018, 3, 20)}
-      startDate={new Date(2018, 3, 30)}
-      endDate={new Date(2018, 4, 5)}
-      theme={{
-        activeDayColor: '',
-        monthTitleTextStyle: {
-          color: '#6d95da',
-          fontWeight: '300',
-          fontSize: 16,
-        },
-        emptyMonthContainerStyle: {},
-        emptyMonthTextStyle: {
-          fontWeight: '200',
-        },
-        weekColumnsContainerStyle: {},
-        weekColumnStyle: {
-          paddingVertical: 10,
-        },
-        weekColumnTextStyle: {
-          color: '#b6c1cd',
-          fontSize: 13,
-        },
-        nonTouchableDayContainerStyle: {},
-        nonTouchableDayTextStyle: {},
-        startDateContainerStyle: {},
-        endDateContainerStyle: {},
-        dayContainerStyle: {},
-        dayTextStyle: {
-          color: '#2d4150',
-          fontWeight: '200',
-          fontSize: 15,
-        },
-        dayOutOfRangeContainerStyle: {},
-        dayOutOfRangeTextStyle: {},
-        todayContainerStyle: {},
-        todayTextStyle: {
-          color: '#6d95da',
-        },
-        activeDayContainerStyle: {
-          backgroundColor: '#6d95da',
-        },
-        activeDayTextStyle: {
-          color: 'white',
-        },
-        nonTouchableLastMonthDayTextStyle: {},
-      }}
-    />
+    <View style={styles.container}>
+
+      <Calendar
+      
+        onPress={(range) => console.log(range)}
+        // minDate={new Date(2018, 3, 20)}
+        startDate={new Date(year, month, date)}
+        // endDate={new Date(2018, 4, 5)}
+      />
+    </View>
   );
 }
---
+++ app/src/pages/calendario/style.tsx
@@ -0,0 +1,10 @@
+
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container:{
+        marginTop: 50,
+        paddingHorizontal: 10,
+    },
+    
+});
