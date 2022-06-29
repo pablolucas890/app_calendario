1cd4ed00b32012678959822550e5bef1fd548507
Maria Clara <clarasanchez2398@gmail.com>
2022-06-29 12:01:49 -0300

    Estilização do calendário
    Co-Authored-By: Jádyla <jadylacesariof@gmail.com>
    Co-Authored-By: Pablo <pablolucas890@gmail.com>
    Co-Authored-By: Samara <samara.quintiliano76@gmail.com>
    Co-Authored-By: Gabriel <gabriel.boloks@gmail.com>

--- app/src/pages/calendario/index.tsx
+++ app/src/pages/calendario/index.tsx
@@ -72,6 +72,49 @@ export function Calendario() {
         firstDayMonday={false}
         monthHeight={300}
         numberOfMonths={1}        // endDate={new Date(2018, 4, 5)}
+        theme={{
+          activeDayColor: 'red',
+          monthTitleTextStyle: {
+            color: '#6d95da',
+            fontWeight: '300',
+            fontSize: 16,
+          },
+          emptyMonthContainerStyle: {},
+          emptyMonthTextStyle: {
+            fontWeight: '200',
+          },
+          weekColumnsContainerStyle: {},
+          weekColumnStyle: {
+            paddingVertical: 10,
+          },
+          weekColumnTextStyle: {
+            color: '#b6c1cd',
+            fontSize: 13,
+          },
+          nonTouchableDayContainerStyle: {},
+          nonTouchableDayTextStyle: {},
+          startDateContainerStyle: {},
+          endDateContainerStyle: {},
+          dayContainerStyle: {},
+          dayTextStyle: {
+            color: '#2d4150',
+            fontWeight: '200',
+            fontSize: 15,
+          },
+          dayOutOfRangeContainerStyle: {},
+          dayOutOfRangeTextStyle: {},
+          todayContainerStyle: {},
+          todayTextStyle: {
+            color: '#6d95da',
+          },
+          activeDayContainerStyle: {
+            backgroundColor: '#6d95da',
+          },
+          activeDayTextStyle: {
+            color: 'white',
+          },
+          nonTouchableLastMonthDayTextStyle: {},
+        }}
       />
       <FlatList
         data={eventsByday}
