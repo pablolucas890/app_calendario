070319037014a7c57c673b59bda83a4158ba7337
igorflpaula <igor_paula12@hotmail.com>
2022-07-13 12:17:07 -0300

    Cadastro Parcial
    Co-Authored-By: Gabriel <gabriel.boloks@gmail.com>

--- app/src/pages/evento/index.tsx
+++ app/src/pages/evento/index.tsx
@@ -1,15 +1,64 @@
+import {
+    KeyboardAvoidingView,
+    TouchableWithoutFeedback,
+    Keyboard,
+    ScrollView,
+    View,
+    Alert,
+  } from "react-native";
+
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { View } from "react-native";
+import { useState } from "react";
 import { Title } from "../../components/Title";
+import { Input } from "../../components/input";
+import { TextGray } from "../../components/TextGray";
+import { Buttom } from "../../components/Buttom";
 import { RootStackParamList } from "../../global/props";
+import { styles } from "../login/style";
 
 type Props = NativeStackScreenProps<RootStackParamList>;
 
 export function evento({ navigation } : Props){
+
+    const [titulo, setTitulo] = useState('');
+    const [descricao, setDesc] = useState('');
+
+    function handleback(){
+        navigation.navigate("Index");
+    }
+
     return (
-        <View>
-            <Title title="Cadastrar " sizeReturn={35} />
-            <Title title="Evento" sizeReturn={35}/>
-        </View>
+        <KeyboardAvoidingView behavior={"height"} style={{ flex: 1 }}>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <ScrollView style={{ flex: 1 }}>
+                    <View style={styles.container}>
+                        <View>
+                            <Title title="Cadastrar Evento" sizeReturn={35} />
+                        </View>
+                        <View style={styles.middle}>
+                            <Input
+                                onChangeText={(value) => {
+                                    setTitulo(value);
+                                }}
+                                placeholder="Ex.: Palestra de Física"
+                                keyboardType="default"
+                            />
+                            <Input
+                                onChangeText={(value) => {
+                                    setDesc(value);
+                                }}
+                                placeholder="Insira a descrição do evento"
+                                keyboardType="default"
+                            />
+                        </View>
+                        <View style={styles.middle2}>
+                            <Buttom color={true} title="Entrar"/>
+                            <View style={{ height: 15 }}></View>
+                            <TextGray onPress={handleback} text="Cancelar" />
+                        </View>
+                    </View>
+                </ScrollView>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     );
 }
